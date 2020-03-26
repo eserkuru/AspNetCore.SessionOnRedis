46c3bdb52bbb5ac808c78bb24d85c33b98f5a632
Eser Kuru <eserkuru@users.noreply.github.com>
2020-03-26 11:04:28 +0300

    Initial commit

---
+++ AspNetCore.SessionOnRedis/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AspNetCore.SessionOnRedis.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace AspNetCore.SessionOnRedis.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Add()
+        {
+            var instance = Dns.GetHostName();
+
+            HttpContext.Session.SetString("KeyOfSessionValue", $"Some nice value added in the instance {instance}, maybe an object serialized to JSON?");
+
+            await HttpContext.Session.CommitAsync();
+
+            ViewData["Message"] = "If you see this, I guess the data was stored ";
+
+            return View();
+        }
+
+        public async Task<IActionResult> Retrieve()
+        {
+
+            await HttpContext.Session.LoadAsync();
+
+            string storedValue = HttpContext.Session.GetString("KeyOfSessionValue");
+
+            if (String.IsNullOrEmpty(storedValue))
+            {
+                storedValue = "What a shame, no session value for you.";
+            }
+
+            var instance = Dns.GetHostName();
+
+            ViewData["Message"] = $"{storedValue} And you are in instance: {instance}";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCore.SessionOnRedis/Startup.cs
@@ -0,0 +1,84 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Redis;
+
+namespace AspNetCore.SessionOnRedis
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            var redisHostUri = "172.19.0.2:6379";
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+
+            var redis = ConnectionMultiplexer.Connect(redisHostUri);
+
+            services
+                .AddDataProtection()
+                .PersistKeysToStackExchangeRedis(redis, "DataProtectionKeys");
+
+            services.AddStackExchangeRedisCache(option =>
+            {
+                option.Configuration = redisHostUri;
+                option.InstanceName = "RedisInstance";
+            });
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+                options.Cookie.Name = "AppTest";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseSession();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
